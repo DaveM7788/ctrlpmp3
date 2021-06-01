9eb8fdf2414086a817a5ff1e332caf5d40f96491
blue <david.mcdermott98@gmail.com>
2021-06-01 19:09:42 -0400

    Removing email cruft
    No longer storing email in database. These variables and function are no longer needed

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -3,12 +3,7 @@ class Constants {
 	// static = do not have to create instance of class first
 	public static $passwordsDoNotMatch = "Your passwords do not match";
 	public static $passwordCharacters = "Your password must be between 6 and 40 characters";
-	public static $emailInvalid = "Email format is invalid";
-	public static $emailsDoNotMatch = "Your emails do not match";
 	public static $usernameCharacters = "Your username must be between 5 and 25 characters";
-	
-	public static $usernameTaken = "This username has already been taken";
-	public static $emailTaken = "This email has already been taken";
 
 	public static $loginFailed = "Your username or password was incorrect";
 }
--- includes/classes/User.php
+++ includes/classes/User.php
@@ -11,10 +11,4 @@ class User {
 	public function getUsername() {
 		return $this->username;
 	}
-
-	public function getEmail() {
-		$query = mysqli_query($this->con, "SELECT email FROM users WHERE username='$this->username'");
-		$row = mysqli_fetch_array($query);
-		return $row['email'];
-	}
 }
