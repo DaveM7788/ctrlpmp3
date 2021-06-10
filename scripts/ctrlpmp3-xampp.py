0c64c4b3c6b3a8627dc1bc3b366bc19f78d6904d
blue <david.mcdermott98@gmail.com>
2021-06-10 19:08:06 -0400

    Simple updates to quick open script
    Adds skeleton for OS X and Linux. Needs to be implemented. Should be easy?

--- scripts/ctrlpmp3-xampp.py
+++ scripts/ctrlpmp3-xampp.py
@@ -1,8 +1,20 @@
 import webbrowser
 import subprocess
+import platform
 
-apache = "C:/xampp/apache_start.bat"
-maria = "C:/xampp/mysql_start.bat"
-subprocess.Popen(apache)
-subprocess.Popen(maria)
-webbrowser.open("http://localhost/ctrlpmp3")
+def open_ctrlp(apache, maria):
+    print("Starting Ctrl-P MP3 ....")
+    subprocess.Popen(apache)
+    subprocess.Popen(maria)
+    webbrowser.open("http://localhost/ctrlpmp3")
+
+if platform.system() == "Windows":
+    apache = "C:/xampp/apache_start.bat"
+    maria = "C:/xampp/mysql_start.bat"
+    open_ctrlp(apache, maria)
+elif platform.system() == "Darwin":
+    print("OS X not implemented yet")
+elif platform.system() == "Linux":
+    print("Linux not implemented yet")
+else:
+    print("Platform is not supported. Exiting")
