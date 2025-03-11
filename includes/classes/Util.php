919b80217a31265ab20d1122ad15cfe5a35f1f26
DaveM7788 <david.mcdermott98@gmail.com>
2025-03-11 01:08:37 -0500

    add util.php

---
+++ includes/classes/Util.php
@@ -0,0 +1,8 @@
+<?php
+class Util {
+	// static = do not have to create instance of class first
+	public static function hashCsrf() {
+        $csrfToken = $_SESSION['csrfToken'];
+        return hash_hmac('sha256', 'util hash hmac', $csrfToken);
+    }
+}
