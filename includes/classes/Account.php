5b419d45b344a2744b3ff7fee2695f2e54900f77
DaveM7788 <david.mcdermott98@gmail.com>
2026-01-13 23:04:41 -0600

    optional type for username

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,40 +1,47 @@
 <?php
-class Account {
-	private $errorArray = array();
-	private $con;
-	
-	public function __construct($con) {
-		$this->con = $con;
-	}
-	
-	public function login($un, $pw) {
-		$stmt = $this->con->prepare("SELECT * FROM users WHERE username=? LIMIT 1");
-		$stmt->bind_param("s", $un);
-		$stmt->execute();
-		$result = $stmt->get_result();
-		$stmt->close();
-		if ($result->num_rows > 0) {
-			$pwFromDB = $result->fetch_assoc();
-			if (password_verify($pw, $pwFromDB['password'])) {
-				return true;
-			} else {
-				array_push($this->errorArray, Constants::$loginFailed);
-				return false;
-			}
-		} else {
-			array_push($this->errorArray, Constants::$loginFailed);
-			return false;
-		}
-	}
+class Account
+{
+    private $errorArray = [];
+    private $con;
 
-	public function getError($error) {
-		if (!in_array($error, $this->errorArray)) {
-			$error = "";
-		}
-		return "<span class='errorMessage'>$error</span>";
-	}
+    public function __construct($con)
+    {
+        $this->con = $con;
+    }
 
-	public function setLoginFailureErrorCsrf() {
-		array_push($this->errorArray, Constants::$loginFailedCsrf);
-	}
+    public function login($un, $pw)
+    {
+        $stmt = $this->con->prepare(
+            "SELECT * FROM users WHERE username=? LIMIT 1",
+        );
+        $stmt->bind_param("s", $un);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        if ($result->num_rows > 0) {
+            $pwFromDB = $result->fetch_assoc();
+            if (password_verify($pw, $pwFromDB["password"])) {
+                return true;
+            } else {
+                array_push($this->errorArray, Constants::$loginFailed);
+                return false;
+            }
+        } else {
+            array_push($this->errorArray, Constants::$loginFailed);
+            return false;
+        }
+    }
+
+    public function getError($error)
+    {
+        if (!in_array($error, $this->errorArray)) {
+            $error = "";
+        }
+        return "<span class='errorMessage'>$error</span>";
+    }
+
+    public function setLoginFailureErrorCsrf()
+    {
+        array_push($this->errorArray, Constants::$loginFailedCsrf);
+    }
 }
--- includes/classes/User.php
+++ includes/classes/User.php
@@ -1,14 +1,17 @@
 <?php
-class User {
-	private $con;
-	private $username;
+class User
+{
+    private $con;
+    private string $username;
 
-	public function __construct($con, $username) {
-		$this->con = $con;
-		$this->username = $username;
-	}
+    public function __construct($con, $username)
+    {
+        $this->con = $con;
+        $this->username = $username;
+    }
 
-	public function getUsername() {
-		return $this->username;
-	}
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
 }
